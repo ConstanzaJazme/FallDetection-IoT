8857ae6d46998fe33a38df903cc584c09559e06b
constanza.jazmeh@utem.cl <constanza.jazmeh@utem.cl>
2020-07-22 00:19:39 -0300

    Signals to measure and setTriggers ok

--- components/2.MPU6050/collecting_data/collecting_data.ino
+++ components/2.MPU6050/collecting_data/collecting_data.ino
@@ -10,7 +10,9 @@
 // ===                   DEFINES AND CONSTANTS                  ===
 // ================================================================
 #define  SAMPLE_FREQUENCY  100      // Frequency to collect data [ms]
-#define BUFFER_LEN 127
+#define IMPOSIBLE_LEN 1000      //This is a value to reset the abnormalTreshold
+#define BUFFER_TiLEN 100        //For Tilt
+#define BUFFER_TrLEN 130        //For Treshold
 
 #define CONSTANT_G 9.81   //Gravity constant
 //Conversion factor (w_ACCorGYRO=actual_reading*(maximum_absolute_ACCorGYRO/maximum_absolute_values_MPU6050)) = actual_reading*(1/(maximum_absolute_values_MPU6050/maximum_absolute_ACCorGYRO)))
@@ -22,9 +24,7 @@
 #define LOWER_TRESHOLD (0.4 * CONSTANT_G)
 #define UPPER_TRESHOLD (2 * CONSTANT_G)
 
-#define MAX_UINT16 1000
-#define MAX_INT16 1000
-#define MAX_FLOAT 1000.0
+#define WAITING_TIME 10000
 
 //This offsets​were obtained from the execution of the example "MPU6050_Calibration" on the "MPU6050" library.
 const int ACC_OFFSET_X = -1449;
@@ -36,7 +36,6 @@ const int GYRO_OFFSET_Y = -23;
 const int GYRO_OFFSET_Z = 35;
 
 const int MPU_addr=0x68;  // I2C address of the MPU-6050
-
 
 
 // ================================================================
@@ -74,10 +73,14 @@ enum States {
 
 States Signal;
 MPU6050 sensor(MPU_addr);
-IMU_rotation vector_tilt[BUFFER_LEN];
-IMU_rotation Current_orientation;
+IMU_rotation current_orientation;
+IMU_rotation vector_tilt[BUFFER_TiLEN];
+IMU_accel vector_treshold[BUFFER_TrLEN];
 
-uint16_t mBuffer_index = 0;
+uint16_t buffer_index = 0;
+int previous_abnormal=0;
+int start_abnormal = 0;
+int end_abnormal = IMPOSIBLE_LEN;
  
 // RAW data from accelerometer and giroscope (x, y and z axis)
 int16_t AcX, AcY, AcZ, Tmp , GyX, GyY, GyZ;
@@ -90,6 +93,10 @@ float Raw_HM;       //For horizontal magnitude (2 axis, y & z)
 float Aangle_roll, Aangle_pitch; 
 
 unsigned  long  mCurrent_time ;
+bool waitSignal=false;      //After a fall detected, this program will wait a time to restart
+
+filter_rotY=0.0;
+filter_rotZ=0.0;
 
 // ================================================================
 // ===                           SETUP                          ===
@@ -126,30 +133,61 @@ void setup() {
  
 void loop() {
     switch (Signal)
-    {
+    {        
         case readingMPU6050:
+            //wait 100 ms to continue
             while (( unsigned  long ) ( millis () - mCurrent_time ) <= SAMPLE_FREQUENCY);
-            mpu_read();     //Read Data collected by MPU6050
             mCurrent_time = millis();
+            //Read Data collected by MPU6050
+            mpu_read();    
+
+            //Processing data from Gyroscope and Accelerometer 
             IMU_accel current_accel = setAccel();   //Process data with the respective ratios values
             IMU_rotation current_angles= processAccelAngles(&current_accel);       //get Aceleration Magnitude, Pitch and Roll angles
-            Raw_AM = (float)sqrt(pow(current_accel->x,2)+pow(current_accel->y,2)+pow(current_accel->z,2));     //Calculate the total Accelerometer Vector or Acceleration Magnitude
+            
+            //Calculate the total Accelerometer Vector or Acceleration Magnitude
+            Raw_AM = (float)sqrt(pow(current_accel->x,2)+pow(current_accel->y,2)+pow(current_accel->z,2));    
             Raw_HM = (float)sqrt(pow(current_accel->y,2)+pow(current_accel->z,2));
 
-            // Check if acceleration above the threshold
-            if (Raw_HM > ACCEL_THRESHOLD) {
+            // Apply Low Pass IIR Filter to roll/pitch angles
+            filter_rotY = FilterLowPass(current_angles.pitch, filter_rotY);
+            filter_rotZ = FilterLowPass(current_angles.roll, filter_rotZ);
+
+            // Check if acceleration lower than threshold
+            if (Raw_AM <= LOWER_TRESHOLD) {
                 Signal = abnormalTreshold;
             }
+
+            // Check the interval recolected after abnormalTreshold
+            if (buffer_index == end_abnormal) {
+                Signal = checkSignal;
+            }   
             
             break;
 
-        case abnormalTreshold:      //when an abnormal event has occurred 
+        case abnormalTreshold:      //when an abnormal event has occurred
+            previous_abnormal = GetBufferPosition(buffer_index, -10);   //Equal to 1 second before lower treshold detected
+            start_abnormal = buffer_index;
+            end_abnormal = GetBufferPosition(buffer_index, 5);      //Equal to 0.5 second after lower treshold detected
+            
+            //Go back to readingMPU6050
+            Signal = readingMPU6050;
             break;
     
         case normalTreshold:       //Transition beetween an abnormal to normal reading
+            //index to 0
+            mMag_index = 0;
+           
+            // Continue measuring accel
+            mState = stateMeasureAccel;
             break;
 
-        case checkSignal:       //verifying abnormal event    
+        case checkSignal:       //verifying abnormal event
+            //Compare min and max amplitudes
+
+            //
+            // ***else*** No Fall Detected so reset the trigger points
+            mState = stateResetTriggers;    
             break;
 
         case checkOrientation:      //verifying abnormal event and orientation change
@@ -158,9 +196,21 @@ void loop() {
         case FallDetected:
         {
             Serial.println("Fall Detected");
+            waitSignal=true;
             Signal = resetTriggers;
             break;
         }
+    }
+
+    // IMPORTANT! only increment buffer when state is measuring data
+    if (Signal == stateMeasureAccel) {
+        buffer_index = GetBufferPosition(buffer_index, 1);  
+    }
+
+    //If a fall is detected, the program will be stopped by a time
+    if (waitSignal==true){
+        while (( unsigned  long ) ( millis () - mCurrent_time ) <= WAITING_TIME);
+        waitSignal=false;
     }
 }
 
@@ -170,7 +220,6 @@ void loop() {
 
 IMU_accel setAccel() {
     Imu_accel accel;
-
     accel.x = AcX * (CONSTANT_G/ACC_RATIOS);       //This values are measured on [m/s2], to work with g measure change CONSTANT_G to 1
     accel.y = AcY * (CONSTANT_G/ACC_RATIOS);
     accel.z = AcZ * (CONSTANT_G/ACC_RATIOS);
@@ -187,12 +236,10 @@ IMU_accel setAccel() {
 // }
 
 IMU_rotation processAccelAngles(Imu_accel* accel){
-    Aangle_pitch = atan(-1 * accel->z / sqrt(pow(accel->x, 2) + pow(accel->y, 2))) * RAD_TO_DEG;      //Calculate the pitch angle
-    Aangle_roll= atan(-1 * accel->y / sqrt(pow(accel->x, 2) + pow(accel->z, 2))) * RAD_TO_DEG;       //Calculate the roll angle
-    
     IMU_rotation angles;
-    angles.roll = Aangle_pitch;
-    angles.pitch = Aangle_pitch;
+    angles.roll= atan(-1 * accel->y / sqrt(pow(accel->x, 2) + pow(accel->z, 2))) * RAD_TO_DEG;       //Calculate the roll angle
+    angles.pitch = atan(-1 * accel->z / sqrt(pow(accel->x, 2) + pow(accel->y, 2))) * RAD_TO_DEG;      //Calculate the pitch angle
+    
     return angles;
 }
 
@@ -229,4 +276,24 @@ void processGyroMagnitudes() {
 
 void processGyroAngles(){
     Raw_GM = sqrt(pow(gx,2)+pow(gy,2)+pow(gz,2));     //Calculate the total Gyroscope Vector or Rotation Magnitude
+}
+
+float FilterLowPass(float new_input, float old_output) {
+    // IIR Low pass Filter y[n] = 0.98y[n-1] + 0.02x[n]
+    return (0.98 * old_output + 0.02 * new_input);
+
+}
+
+int GetBufferPosition(int current_pos, int samples) {       //Get the position given a certain point in the array
+  int buffer_pos = current_pos + samples;
+
+  if (buffer_pos > BUFFER_LEN - 1) {
+    buffer_pos -= BUFFER_LEN;
+  }
+
+  if (buffer_pos < 0) {
+    buffer_pos += BUFFER_LEN;
+  }
+  return (int) buffer_pos;
+
 }
